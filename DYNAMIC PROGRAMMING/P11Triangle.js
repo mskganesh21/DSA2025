9b88145ddb4ae1b7fb9786c1caac441a43946c87
Sai krishna Ganesh <157506559+mskganesh21@users.noreply.github.com>
2025-07-10 16:13:23 +0530

    Create P11Triangle.js

---
+++ DYNAMIC PROGRAMMING/P11Triangle.js
@@ -0,0 +1,24 @@
+/*
+DP ON GRIDS
+TRIANGLE
+variable starting and ending points
+*/
+/*
+TRIANGLE PROBLEM
+given a 2d array in the form of a triangle
+calculate the minimum path sum to reach the bottom from the top.
+it is given that the ith row with have i+1 elements
+we can either move down 
+*/
+/*
+ALGO: 
+
+*/
+const arr = [[1],[2,3],[3,6,7],[8,9,6,10]];
+
+const Triangle = (arr) => {
+
+//length of the rows
+let r = arr.length;
+};
+
