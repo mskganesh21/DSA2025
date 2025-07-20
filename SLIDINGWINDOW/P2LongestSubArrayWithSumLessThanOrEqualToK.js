3fab2a557a96bbeb146dc400a78531b16cf32cb6
Sai krishna Ganesh <157506559+mskganesh21@users.noreply.github.com>
2025-07-20 17:55:05 +0530

    Update P2LongestSubArrayWithSumLessThanOrEqualToK.js

--- SLIDINGWINDOW/P2LongestSubArrayWithSumLessThanOrEqualToK.js
+++ SLIDINGWINDOW/P2LongestSubArrayWithSumLessThanOrEqualToK.js
@@ -57,3 +57,36 @@ const LongestSubArrayWithSumLessThanOrEqualToK1 = (arr,k) => {
   return maxLength;
 };
 console.log(LongestSubArrayWithSumLessThanOrEqualToK1(arr,k));
+
+/*
+OPTIMAL SOLUTION
+*/
+/*
+use 2 pointers and sliding window
+initially keep window size as 1
+then keep increasing the window size until the condition
+and then if it crosses the condition then shrink the window
+in this way we can get SOLUTION
+*/
+const longestSubarrayWithSumLessThanOrEqualToK1 = (arr, k) => {
+  let n = arr.length;
+  let l = 0, r = 0, sum = 0, maxLength = 0;
+
+  while (r < n) {
+    sum += arr[r];
+
+    // Shrink window until sum <= k
+    while (sum > k && l <= r) {
+      sum -= arr[l];
+      l++;
+    }
+
+    // Now sum <= k
+    maxLength = Math.max(maxLength, r - l + 1);
+
+    // Expand window
+    r++;
+  }
+
+  return maxLength;
+};
